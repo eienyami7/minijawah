0df59b50ee13705306bacbbe96b8de20e6033b8c
eienyami7 <eienyami7@gmail.com>
2022-04-03 20:13:32 +0530

    Added cooldown on the commands, added ?bed and finished ?title

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from twitchio.ext import commands
-import random
+import math
 import os
-from dotenv import load_dotenv
-import requests
+import random
+import time
 
+import requests
+from dotenv import load_dotenv
+from twitchio.ext import commands
 
 load_dotenv('.env')
 AUTH_TOKEN = os.environ['TMI_TOKEN']
 BOT_CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_ID = "9ggr8vllhbakby13wq85giayqykkfx"
+CLIENT_SECRET = "edq9plpecwlny3d0095i8lgliz5f8u"
+JAWAH_AUTH_TOKEN = os.environ['JAWAH_AUTH_TOKEN']
 nick = os.environ['BOT_NICK']
 prefix = os.environ['BOT_PREFIX']
 initial_channels = os.environ['CHANNEL'].split(",")
@@ -15,11 +20,13 @@ API_URL = "https://api.twitch.tv/kraken/channels/"
 DISCORD_URL = "https://discord.com/invite/h3yWGf3"
 TWITTER_URL = "https://twitter.com/JawahTV"
 YOUTUBE_URL = "https://www.youtube.com/channel/UC0Uui0gxffT5p8HTqUp1e1g"
+COMMAND_TIME_COOLDOWN = 5
 
 
 class Bot(commands.Bot):
     def __init__(self):
         super().__init__(token=AUTH_TOKEN, prefix=prefix, initial_channels=initial_channels)
+        self.last_invocation_time = dict()
 
     async def event_ready(self):
         print(f'Logged in as | {self.nick}')
@@ -32,29 +39,68 @@ class Bot(commands.Bot):
         # print(message.content)
         await self.handle_commands(message)
 
+    def cooldown_checker(self, input_command):
+        if input_command not in self.last_invocation_time:
+            self.last_invocation_time[input_command] = time.time()
+        else:
+            time_diff = time.time() - self.last_invocation_time[input_command]
+            if time_diff < COMMAND_TIME_COOLDOWN:
+                return f"Please wait for {math.ceil(COMMAND_TIME_COOLDOWN - time_diff)} second(s) " \
+                       f"before using the command again."
+        self.last_invocation_time[input_command] = time.time()
+        return False
+
     @commands.command(name="lookjake")
     async def lookjake(self, ctx: commands.Context):
+        is_on_cooldown = self.cooldown_checker("lookjake")
+        if is_on_cooldown:
+            await ctx.send(is_on_cooldown)
         randomizer = random.randint(1, 5)
         if randomizer == 5:
-            response_string = "Shut up Fide"
+            response_string = "Shut up @" + ctx.author.name
         elif randomizer % 2 == 0:
-            response_string = 'Its Luna with pirate hat'
+            response_string = 'It is Luna but with pirate hat!'
         elif randomizer % 2 == 1:
             response_string = 'IODabs'
         await ctx.send(response_string)
 
+    @commands.command(name="bed")
+    async def bed(self, ctx: commands.Context):
+        is_on_cooldown = self.cooldown_checker("bed")
+        if is_on_cooldown:
+            await ctx.send(is_on_cooldown)
+        randomizer = random.randint(1, 5)
+        message = str(ctx.message.content)
+        if len(message.split(" ")) == 1:
+            response_string = "Go to bed Roger!"
+        else:
+            if randomizer == 5:
+                response_string = "How about you go to bed @" + ctx.author.name
+            else:
+                response_string = "Go to bed " + message.split(' ', 1)[1]
+        await ctx.send(response_string)
+
     @commands.command(name="discord")
     async def discord(self, ctx: commands.Context):
+        is_on_cooldown = self.cooldown_checker("discord")
+        if is_on_cooldown:
+            await ctx.send(is_on_cooldown)
         response_string = f"Join Discord: {DISCORD_URL}"
         await ctx.send(response_string)
 
     @commands.command(name="twitter")
     async def twitter(self, ctx: commands.Context):
+        is_on_cooldown = self.cooldown_checker("twitter")
+        if is_on_cooldown:
+            await ctx.send(is_on_cooldown)
         response_string = f"My Twitter: {TWITTER_URL}"
         await ctx.send(response_string)
 
     @commands.command(name="youtube")
     async def youtube(self, ctx: commands.Context):
+        is_on_cooldown = self.cooldown_checker("youtube")
+        if is_on_cooldown:
+            await ctx.send(is_on_cooldown)
         response_string = f"My YouTube: {YOUTUBE_URL}"
         await ctx.send(response_string)
 
@@ -67,19 +113,18 @@ class Bot(commands.Bot):
             else:
                 title = message.split(' ', 1)[1]
                 channel_id = ctx.channel.name
-                url = API_URL + channel_id
-                headers = \
-                    {
-                        'Accept': 'application/vnd.twitchtv.v5+json',
-                        "Client-ID": BOT_CLIENT_ID,
-                        "Authorization": "OAuth " + AUTH_TOKEN,
-                        "Content-Type": "application/json"
-                    }
-                data = f'{{"channel": {{"status":"{title}"}}}}'
+                url = 'https://api.twitch.tv/helix/channels?broadcaster_id=' + channel_id
+                headers = {
+                    'Authorization': JAWAH_AUTH_TOKEN,
+                    'Client-Id': CLIENT_ID,
+                    'Content-Type': 'application/json'
+                }
+                data = f'{{"title":"{title}"}}'
                 response = requests.put(url=url, headers=headers, data=data.encode('utf-8')).json()
                 if response.status_code == 200:
                     await ctx.send(f'Title successfully changed to -> "{title}"')
-                # await ctx.send(f'Title successfully changed to -> "{title}" for channel {channel_id}')
+
+# TODO: Add game change command ?game
 
 
 if __name__ == "__main__":
